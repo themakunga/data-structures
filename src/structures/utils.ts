b65fc8bb23d2e1a2dc442f9b01bb1e7350e8dc16
Nicolas Martinez V <nmartinezv@icloud.com>
2021-10-15 09:58:33 -0300

    wip: trees

--- src/structures/utils.ts
+++ src/structures/utils.ts
@@ -13,12 +13,12 @@ export const defaultEquals = <T>(a: T, b: T): boolean => a === b;
 
 export const VALUE_NOT_EXISTS_ERROR = 'Value does not exists';
 
-// /**
-//  * default function to compare elements order.
-//  * @function
-//  */
-// export function defaultCompare<T>(a: T, b: T): number {
-//   if (a < b) return -1;
-//   if (a === b) return 0;
-//   return 1;
-// }
+/**
+ * default function to compare elements order.
+ * @function
+ */
+export function defaultCompare<T>(a: T, b: T): number {
+  if (a < b) return -1;
+  if (a === b) return 0;
+  return 1;
+}
